f733d9e16b9703557dba01d8d97e8940d2c6d232
William Giguere <willgiguere@gmail.com>
2021-05-08 21:02:13 -0400

    Added stats, deck, and artifacts to precombat screen.

--- TowerDefense/src/scenes/PreCombatScreen.cpp
+++ TowerDefense/src/scenes/PreCombatScreen.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "PreCombatScreen.h"
 #include "TowerDefense.h"
+#include "core/Player.h"
 
 TowerDefense::PreCombatScreen::PreCombatScreen()
 	:m_BeginCombat(std::make_unique<Button>(400.0f, 300.0f, 180, 50, "beginCombatButton")
@@ -11,14 +12,24 @@ TowerDefense::PreCombatScreen::PreCombatScreen()
 void TowerDefense::PreCombatScreen::Render()
 {
 	m_BeginCombat->Render();
+	
+	Player& player = Player::Get();
+	player.RenderStats();
+	player.RenderDeckAndArtifacts();
 }
 
 void TowerDefense::PreCombatScreen::Update()
 {
-	m_BeginCombat->Update();
+	Player& player = Player::Get();
+	player.UpdateDeckAndArtifacts();
 
-	if (m_BeginCombat->IsClicked())
-		TowerDefense::SetScene(SceneType::COMBAT);
+	if (!player.DeckShowing() && !player.ArtifactsShowing())
+	{
+		m_BeginCombat->Update();
+
+		if (m_BeginCombat->IsClicked())
+			TowerDefense::SetScene(SceneType::COMBAT);
+	}
 }
 
 void TowerDefense::PreCombatScreen::OnSwitch()
