fad1e898f6bd257711379c304a1ae3da8a7ba56b
William Giguere <willgiguere@gmail.com>
2021-05-30 17:27:16 -0400

    Clerics and bards now scale with magic damage

--- TowerDefense/src/towers/Bard.cpp
+++ TowerDefense/src/towers/Bard.cpp
@@ -7,24 +7,24 @@
 TowerDefense::Tower::Bard::Bard()
 	:Tower(0.0f, 0.0f, 32, 32, 60.0f, 75, TowerType::SUPPORT, "Bard")
 {
-	m_AttackSpeedBuff = 1.25f;
+	m_MagicDamage = 1.0f;
 }
 
 TowerDefense::Tower::Bard::Bard(float fireTime, int range, float buff)
 	: Tower(0.0f, 0.0f, 32, 32, fireTime, range, TowerType::SUPPORT, "Bard")
 {
-	m_AttackSpeedBuff = buff;
+	m_MagicDamage = buff;
 }
 
 void TowerDefense::Tower::Bard::Fire(std::shared_ptr<TowerDefense::Entity> target)
 {
 	std::shared_ptr<Tower> tower = std::dynamic_pointer_cast<Tower>(target);
-	tower->ApplyBuff(std::make_shared<SpeedBuff>(150, m_ID, m_AttackSpeedBuff));
+	tower->ApplyBuff(std::make_shared<SpeedBuff>(150, m_ID, 1.0f + m_MagicDamage*0.25f));
 }
 
 std::shared_ptr<TowerDefense::Tower::Tower> TowerDefense::Tower::Bard::Clone()
 {
-	auto tower = std::make_shared<Bard>(GetAttackTime(), GetRange(), m_AttackSpeedBuff);
+	auto tower = std::make_shared<Bard>(GetAttackTime(), GetRange(), m_MagicDamage);
 	tower->SetDamage(m_PhysicalDamage, DamageType::PHYSICAL);
 	tower->SetDamage(m_MagicDamage, DamageType::MAGIC);
 	tower->SetCritChance(m_CritChance);
--- TowerDefense/src/towers/Cleric.cpp
+++ TowerDefense/src/towers/Cleric.cpp
@@ -7,25 +7,25 @@
 TowerDefense::Tower::Cleric::Cleric()
 	:Tower(0.0f, 0.0f, 32, 32, 60.0f, 75, TowerType::SUPPORT, "Cleric")
 {
-	m_DamageBuff = 1.0f;
+	m_MagicDamage = 1.0f;
 }
 
 TowerDefense::Tower::Cleric::Cleric(float fireTime, int range, float buff)
 	: Tower(0.0f, 0.0f, 32, 32, fireTime, range, TowerType::SUPPORT, "Cleric")
 {
-	m_DamageBuff = buff;
+	m_MagicDamage = buff;
 }
 
 void TowerDefense::Tower::Cleric::Fire(std::shared_ptr<TowerDefense::Entity> target)
 {
 	std::shared_ptr<Tower> tower = std::dynamic_pointer_cast<Tower>(target);
-	tower->ApplyBuff(std::make_shared<DamageBuff>(150, m_ID, DamageType::MAGIC, m_DamageBuff));
-	tower->ApplyBuff(std::make_shared<DamageBuff>(150, m_ID, DamageType::PHYSICAL, m_DamageBuff));
+	tower->ApplyBuff(std::make_shared<DamageBuff>(150, m_ID, DamageType::MAGIC, m_MagicDamage));
+	tower->ApplyBuff(std::make_shared<DamageBuff>(150, m_ID, DamageType::PHYSICAL, m_MagicDamage));
 }
 
 std::shared_ptr<TowerDefense::Tower::Tower> TowerDefense::Tower::Cleric::Clone()
 {
-	auto tower = std::make_shared<Cleric>(GetAttackTime(), GetRange(), m_DamageBuff);
+	auto tower = std::make_shared<Cleric>(GetAttackTime(), GetRange(), m_MagicDamage);
 	tower->SetDamage(m_PhysicalDamage, DamageType::PHYSICAL);
 	tower->SetDamage(m_MagicDamage, DamageType::MAGIC);
 	tower->SetCritChance(m_CritChance);
