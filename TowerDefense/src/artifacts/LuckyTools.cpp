d82eac6fbc691a9a922ef00126050617209d6310
William Giguere <willgiguere@gmail.com>
2021-05-30 12:55:22 -0400

    Including lucky tools file

---
+++ TowerDefense/src/artifacts/LuckyTools.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "Artifact.h"
+#include "core/Player.h"
+
+TowerDefense::LuckyAxe::LuckyAxe()
+	:Artifact("Lucky Axe")
+{}
+
+void TowerDefense::LuckyAxe::OnAquire()
+{
+	Player::Get().SetResourceGatherRate(Player::Get().GetResourceGatherRate(Resource::WOOD) + 50, Resource::WOOD);
+}
+
+TowerDefense::LuckyPickaxe::LuckyPickaxe()
+	:Artifact("Lucky Pickaxe")
+{}
+
+void TowerDefense::LuckyPickaxe::OnAquire()
+{
+	Player::Get().SetResourceGatherRate(Player::Get().GetResourceGatherRate(Resource::STONE) + 50, Resource::STONE);
+}
+
+TowerDefense::LuckySythe::LuckySythe()
+	:Artifact("Lucky Sythe")
+{}
+
+void TowerDefense::LuckySythe::OnAquire()
+{
+	Player::Get().SetResourceGatherRate(Player::Get().GetResourceGatherRate(Resource::STONE) + 50, Resource::STONE);
+}
