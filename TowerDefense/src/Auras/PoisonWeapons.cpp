69e52945b6da294b275b390c57cb43935dafa213
William Giguere <willgiguere@gmail.com>
2021-05-31 16:11:10 -0400

    Envenom damage now correctly shows as aura damage, not card damage.

--- TowerDefense/src/Auras/PoisonWeapons.cpp
+++ TowerDefense/src/Auras/PoisonWeapons.cpp
@@ -11,5 +11,5 @@ TowerDefense::PoisonWeapons::PoisonWeapons(float poisonDamage, int poisonDuratio
 void TowerDefense::PoisonWeapons::OnEnemyHit(std::shared_ptr<Enemy::Enemy> e, std::shared_ptr<Entity> source, Tower::DamageType type)
 {
 	if (source->GetEntityType() == Type::TOWER && type == Tower::DamageType::PHYSICAL)
-		e->Poison(m_PoisonDamage, m_PoisonDuration, source->GetID());
+		e->Poison(m_PoisonDamage, m_PoisonDuration, GetID());
 }
