d94dd1dc35a4b3eac9a804e44abe7b9531039a19
WillGig <willgiguere@gmail.com>
2022-02-08 16:39:28 -0500

    Map editor now shows which tile is selected and displays that tile over the current tile on the board

--- TowerDefense/src/scenes/MapEditor.h
+++ TowerDefense/src/scenes/MapEditor.h
@@ -15,11 +15,14 @@ namespace TowerDefense
 		void SetMap(int m);
 
 		int m_NumMaps, m_CurrentMap, m_CurrentTile;
+		std::string m_CurrentTileTexture;
 
 		std::array<std::unique_ptr<TowerDefense::Button>, 7> m_Buttons;
 		std::array<std::unique_ptr<TowerDefense::Button>, 6> m_Tiles;
 
 		std::unique_ptr<Text> m_CurrentMapName;
+		std::unique_ptr<Rectangle> m_CurrentTileHighlight;
+		std::unique_ptr<Image> m_PotentialTile;
 
 		std::stack<std::vector<Tile>> m_UndoHistory;
 		std::vector<Tile> m_CurrentChange;
